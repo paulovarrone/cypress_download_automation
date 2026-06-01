accd1d8f54cee14b5489efb780c5f2e5dbc95d00
Paulo Varrone <paulorobertovarrone@gmail.com>
2026-05-31 22:27:49 -0300

    ajuste de funções

--- cypress/support/BaixarDocumentos.js
+++ cypress/support/BaixarDocumentos.js
@@ -8,13 +8,73 @@ Cypress.Commands.add('entrarPaginaDocumentos', () => {
     cy.url().should('include', '/documents');
 })
 
-Cypress.Commands.add('verTodosDocumentos', () => {       
-    cy.contains('a', 'View all PDF →').click();
-})
-
-Cypress.Commands.add('massDownloadDOCS', () => {    
+Cypress.Commands.add('dPDF', () => {   
+    cy.contains('a', 'View all PDF →').click(); 
     for(let i = 0; i < 15; i++) {
         cy.get('.mt-auto.flex.gap-2.pt-5').eq(`${i}`).find('a').contains('Download').click(); 
-    }   
+    } 
+    cy.go('back');  
 })
 
+Cypress.Commands.add('dDOCX', () => {  
+    cy.get('#tab-1').click();  
+    cy.contains('a', 'View all DOCX →').click(); 
+    for(let i = 0; i < 14; i++) {
+        cy.get('.mt-auto.flex.gap-2.pt-5').eq(`${i}`).find('a').contains('Download').click(); 
+    }   
+    cy.go('back');
+})
+
+Cypress.Commands.add('dEPUB', () => {  
+    cy.get('#tab-2').click();  
+    cy.contains('a', 'View all EPUB →').click(); 
+    for(let i = 0; i < 7; i++) {
+        cy.get('.mt-auto.flex.gap-2.pt-5').eq(`${i}`).find('a').contains('Download').click(); 
+    }   
+    cy.go('back');
+})
+
+Cypress.Commands.add('dXLSX', () => {  
+    cy.get('#tab-3').click();  
+    cy.contains('a', 'View all XLSX →').click(); 
+    for(let i = 0; i < 14; i++) {
+        cy.get('.mt-auto.flex.gap-2.pt-5').eq(`${i}`).find('a').contains('Download').click(); 
+    }   
+    cy.go('back');
+})
+
+Cypress.Commands.add('dPPTX', () => {  
+    cy.get('#tab-4').click();  
+    cy.contains('a', 'View all PPTX →').click(); 
+    for(let i = 0; i < 7; i++) {
+        cy.get('.mt-auto.flex.gap-2.pt-5').eq(`${i}`).find('a').contains('Download').click(); 
+    }   
+    cy.go('back');
+})
+
+Cypress.Commands.add('dCSV', () => {  
+    cy.get('#tab-5').click();  
+    cy.contains('a', 'View all CSV →').click(); 
+    for(let i = 0; i < 13; i++) {
+        cy.get('.mt-auto.flex.gap-2.pt-5').eq(`${i}`).find('a').contains('Download').click(); 
+    }   
+    cy.go('back');
+})
+
+Cypress.Commands.add('dMarkdown', () => {  
+    cy.get('#tab-6').click();  
+    cy.contains('a', 'View all Markdown →').click(); 
+    for(let i = 0; i < 8; i++) {
+        cy.get('.mt-auto.flex.gap-2.pt-5').eq(`${i}`).find('a').contains('Download').click(); 
+    }   
+    cy.go('back');
+})
+
+Cypress.Commands.add('dTXT', () => {  
+    cy.get('#tab-7').click();  
+    cy.contains('a', 'View all TXT →').click(); 
+    for(let i = 0; i < 9; i++) {
+        cy.get('.mt-auto.flex.gap-2.pt-5').eq(`${i}`).find('a').contains('Download').click(); 
+    }   
+    cy.go('back');
+})
