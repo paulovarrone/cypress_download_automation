2403a04919f37451d880893ec1b810e0f3407d58
Paulo Varrone <paulorobertovarrone@gmail.com>
2026-06-06 21:24:23 -0300

    testando paralelismo

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
+  projectId: "6pvy75",
   
   e2e: {
     baseUrl: 'https://truefilesize.com/',
